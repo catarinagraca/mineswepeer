c44d40307156e66898ebd999fda40d69025e8a23
catarinagraca <catarinagraca40+github@gmail.com>
2024-05-15 21:24:52 +0100

    control panel e bombs

--- src/componentes/cell/cell.jsx
+++ src/componentes/cell/cell.jsx
@@ -1,26 +1,13 @@
 import React ,{ useState }from "react";
 import './cell.css';
 
-export default function Cell({pos}){
-    
-    const [clicado, setClicado] = useState(false);
-    
-    // Função para lidar com o clique no quadrado
-    const handleClick = () => {
-        // Altera o estado para indicar que o quadrado foi clicado
-        setClicado(true);
-    };
-    //const estiloQuadrado = clicado ? 'quadrado red' : 'quadrado'
-    let estiloQuadrado=''
-    if (pos%2 ===0){
-        estiloQuadrado='odd'
-    }
-    else{
-        estiloQuadrado='even'
+export default function Cell({handleClick,x,y, cell}){
 
+    const handleCellClick=()=>{
+        handleClick(x,y)
     }
 
     return(
-        <button className={estiloQuadrado} onClick={handleClick}></button>
+        <button className="quadrado" onClick={handleCellClick}>{cell.bomb ? "*" : ''}</button>
     )
 }
--- src/componentes/control panel/controlPanel.jsx
+++ src/componentes/control panel/controlPanel.jsx
@@ -1,33 +1,49 @@
-import { useState } from 'react';
-import React from "react";
-import './controlPanel.css'
-import Timer from '../timer/timer';
+import React,{ useEffect, useState } from "react";
+import "./controlPanel.css";
+import Timer from "../timer/timer";
 
-export default function ControlPanel({onLevelChange}){
+export default function ControlPanel({
+  onLevelChange,
+  startClock,
+  handleClick,
+}) {
+  const [gameStarted, setGameStarted] = useState(startClock);
 
+  const handleTimer = () => {
+    setGameStarted(true);
+  };
 
-    const [gameStarted, setGameStarted] = useState(false);
+useEffect(()=> {
     
+}, [startClock])
+  /*const [nivelJogo, setNivelJogo] = useState("básico");*/
 
-    const handleTimer = () => {
-      setGameStarted(true);
-    };
-  
-    /*const [nivelJogo, setNivelJogo] = useState("básico");*/
-    
-    return(
-        <div>
-            <div className='btns'>
-                <button className="levelBtn" onClick={() => onLevelChange('1')} >Básico (9x9)</button>
-                <button className="levelBtn" onClick={() => onLevelChange('2')} >Intermédio (16x16)</button>
-                <button className="levelBtn" onClick={() => onLevelChange('3')} >Avançado (30x16)</button>
-                <button className='startBtn' onClick={handleTimer}>Iniciar Jogo</button> 
-            </div>
-            <dl className='itemsPanel' >
-                <dt>Tempo de Jogo:</dt>
-                <dd id="gameTime">{gameStarted ? <Timer /> : '0'}s</dd> 
-            </dl>
-        </div>
-    )       
-    
+  return (
+    <div>
+      <div className="btns">
+        <button className="levelBtn" onClick={() => onLevelChange("1")}>
+          Básico (9x9)
+        </button>
+        <button className="levelBtn" onClick={() => onLevelChange("2")}>
+          Intermédio (16x16)
+        </button>
+        <button className="levelBtn" onClick={() => onLevelChange("3")}>
+          Avançado (30x16)
+        </button>
+        <button className="restartBtn" onClick={handleClick}>
+          Reiniciar Jogo
+        </button>
+      </div>
+      <div className="itemsPanel">
+        <dl className="items">
+          <dt>Tempo de Jogo:</dt>
+          <dd id="gameTime">{gameStarted ? <Timer /> : "0"}s</dd>
+        </dl>
+        <dl className="items">
+          <dt>Bandeiras:</dt>
+          <dd id="flags">x</dd>
+        </dl>
+      </div>
+    </div>
+  );
 }

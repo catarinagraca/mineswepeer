32a55eade5d0a2ede6b6d4abe26347ad156a6391
catarinagraca <catarinagraca40+github@gmail.com>
2024-05-17 12:36:26 +0100

    funcao conta celulas sem minas, proximity bombs

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import GamePanel from "./componentes/game panel/gamepanel";
 import Header from "./componentes/header/header";
 
 function App() {
-  const [board, setBoard] = useState({    //board dos niveis
+  const [board, setBoard] = useState({
+    //board dos niveis
     nLines: 9,
     nColumns: 9,
     nBombs: 10,
@@ -14,7 +15,8 @@ function App() {
   const [level, setLevel] = useState("1");
   const [gameStarted, setGameStarted] = useState(false);
 
-  const [arrayBoard, setArrayBoard] = useState(() => {      //board bidimensional com bombas, etc...
+  const [arrayBoard, setArrayBoard] = useState(() => {
+    //board bidimensional com bombas, etc...
     const initalBoard = Array.from({ length: board.nLines }, () =>
       Array.from({ length: board.nColumns }, () => ({
         bomb: false,
@@ -26,41 +28,12 @@ function App() {
     return initalBoard;
   });
 
- const handleClick=(x,y)=>{
-    //console.log(x+','+y);
-    setGameStarted(true)
-    if (!gameStarted) {
-      spawnBomb()
-    
-    } 
-    const newBoard = [...arrayBoard];
-    if (newBoard[y][x].bomb==true) {
-            alert("Game Over");
-            handleResetGame()
-            
-             //gameover
-        }
-    
- }
 
- 
-
-  const handleResetGame = () => {
-     setGameStarted(false);
-     setArrayBoard(() => {      //board bidimensional com bombas, etc...
-      const initalBoard = Array.from({ length: board.nLines }, () =>
-        Array.from({ length: board.nColumns }, () => ({
-          bomb: false,
-          flag: false,
-          clicked: false,
-          promixityBombs: 0,
-        }))
-      );
-      return initalBoard;
-    });
-     
-     
-     };
+  const nearbyCells = [
+    [-1, -1], [-1, 0], [-1, 1],
+    [0, -1], [0, 1],
+    [1, -1], [1, 0], [1, 1]
+  ];
 
   const changeBoard = (level) => {
     let newBoard = {};
@@ -91,6 +64,7 @@ function App() {
     changeArrayBoard(newBoard);
     //setarrayBoard(newBoard)
   };
+
   const changeArrayBoard = (board) => {
     const newArrayBoard = Array.from({ length: board.nLines }, () =>
       Array.from({ length: board.nColumns }, () => ({
@@ -104,10 +78,48 @@ function App() {
     return newArrayBoard;
   };
 
-  const handleLevelChange = (event) => {  //event=nivel
+  const handleResetGame = () => {
+    setGameStarted(false);
+    setArrayBoard(() => {
+      //board bidimensional com bombas, etc...
+      const initalBoard = Array.from({ length: board.nLines }, () =>
+        Array.from({ length: board.nColumns }, () => ({
+          bomb: false,
+          flag: false,
+          clicked: false,
+          promixityBombs: 0,
+        }))
+      );
+      return initalBoard;
+    });
+  };
+
+  const handleClick = (x, y) => {
+    if (!gameStarted) {
+      setGameStarted(true);
+      spawnBomb();
+      checkBomb();
+    }
+    const newBoard = [...arrayBoard];
+    if (newBoard[y][x].clicked == false) {
+      newBoard[y][x].clicked = true
+
+    }
+    console.log(newBoard);
+    if (newBoard[y][x].bomb == true) {
+      alert("Game Over");
+      handleResetGame();
+
+
+    }
+    //checkBomb(x,y)
+
+  };
+
+  const handleLevelChange = (event) => {
+    //event=nivel
     setLevel(event);
     changeBoard(event);
-    
   };
 
   const spawnBomb = () => {
@@ -115,36 +127,72 @@ function App() {
     const newBoard = [...arrayBoard];
 
     while (bombs < board.nBombs) {
-      const x = Math.floor(Math.random() * board.nLines);   //gera posições aleatorias
+      const x = Math.floor(Math.random() * board.nLines); //gera posições aleatorias
       const y = Math.floor(Math.random() * board.nColumns);
 
       if (!newBoard[x][y].bomb) {
-        newBoard[x][y].bomb = true; // Marca a posição como uma bomba
+        newBoard[x][y].bomb = true;
+        //console.log(x,y); // Marca a posição como uma bomba
         bombs++; // Adiciona a bomba à lista de bombas
       }
     }
     console.log("bombas geradas");
+    console.log(newBoard);
     setArrayBoard(newBoard);
   };
 
-  
+  const checkBomb = () => {
+    const newBoard = [...arrayBoard];
+    for (let x = 0; x < board.nLines; x++) {
+      for (let y = 0; y < board.nColumns; y++) {
+        let bombCount = 0;
 
-  return (
-    <div id="container">
-      <Header></Header>
-      <ControlPanel
-        onLevelChange={handleLevelChange}
-        handleClick={handleResetGame}
-        startClock={gameStarted}
-        bombs={board.nBombs}
+        if (!newBoard[x][y].bomb) {
+          nearbyCells.forEach(([dx, dy]) => {
+            const neighbourX = x + dx;
+            const neighbourY = y + dy;
+            
+            if (neighbourX >= 0 && neighbourX < board.nColumns && neighbourY >= 0 && neighbourY < board.nLines) {
+              if (arrayBoard[neighbourX][neighbourY].bomb) {
+                bombCount++;
+              }
+            }
+          });
+        }
+        console.log(bombCount);
+        newBoard[x][y] = {      //copia e adiciona o proximity bombs
+          ... newBoard[x][y],
+          proximityBombs: bombCount
+        }
+      }
+    }
 
-        //onGameStart={handleGameStart}
-      ></ControlPanel>
-      <GamePanel board={board} arrayBoard={arrayBoard} level={level} handleClick={handleClick}></GamePanel>
-      
-    </div>
-    // <Cell></Cell>
-  );
+
+  };
+;
+
+
+
+return (
+  <div id="container">
+    <Header></Header>
+    <ControlPanel
+      onLevelChange={handleLevelChange}
+      handleClick={handleResetGame}
+      startClock={gameStarted}
+      bombs={board.nBombs}
+
+    //onGameStart={handleGameStart}
+    ></ControlPanel>
+    <GamePanel
+      board={board}
+      arrayBoard={arrayBoard}
+      level={level}
+      handleClick={handleClick}
+    ></GamePanel>
+  </div>
+  // <Cell></Cell>
+);
 }
 
 export default App;
